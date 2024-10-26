cafbb67b629e272071267e94d99f18252415b544
Finsa Nurpandi <finsaworks@gmail.com>
2024-10-26 10:28:19 +0700

    laravel migration

---
+++ laravel-project/database/migrations/2024_10_26_021337_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->char('npm',10)->unique();
+            $table->string('name', 60);
+            $table->year('year_entry');
+            $table->char('nidn', 10);
+            $table->foreignId('department_id')
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            // $table->timestamps();
+
+            // set FK constraints
+            $table->foreign('nidn')->references('nidn')->on('lecturers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
---
+++ laravel-project/database/migrations/2024_10_26_023926_modify_students_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->renameColumn('name', 'fullname');
+            $table->string('group',2)->after('year_entry');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->renameColumn('fullname', 'name');
+            $table->dropColumn('group');
+        });
+    }
+};
